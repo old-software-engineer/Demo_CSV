5c0ab0938b2833eaa9988c98a53e093777c4e3e4
sharmaparas4444 <sharma.paras4444@gmail.com>
2021-01-27 18:45:08 +0530

    update some changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,15 @@ class HomeController < ApplicationController
   end
 
   def upload_csv
-  	# debugger
   	@email_list = []
   	@csv_file1 = CSV.read(params["csv1"].path)
   	@csv_file2 = CSV.read(params["csv2"].path)
-  	# csv_index = params["concern"] == "" ? 1 : 2
-
 	@csv_file1.each do |x|
 	  	file2_data = @csv_file2.find{|a| a[0] == x[0]}
 	  	if !(file2_data[params['concern'].to_i].include? x[params['concern'].to_i] or x[params['concern'].to_i].include? file2_data[params['concern'].to_i])
 	  		@email_list << file2_data[0]
 	  	end
 	end
-
   end
+
 end
