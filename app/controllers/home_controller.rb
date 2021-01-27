cad9cc5442b61ea1cdb06b2766d7ee76fedc2cb0
sharmaparas4444 <sharma.paras4444@gmail.com>
2021-01-27 18:26:43 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'csv'
+
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def upload_csv
+  	# debugger
+  	@email_list = []
+  	@csv_file1 = CSV.read(params["csv1"].path)
+  	@csv_file2 = CSV.read(params["csv2"].path)
+  	# csv_index = params["concern"] == "" ? 1 : 2
+
+	@csv_file1.each do |x|
+	  	file2_data = @csv_file2.find{|a| a[0] == x[0]}
+	  	if !(file2_data[params['concern'].to_i].include? x[params['concern'].to_i] or x[params['concern'].to_i].include? file2_data[params['concern'].to_i])
+	  		@email_list << file2_data[0]
+	  	end
+	end
+
+  end
+end
